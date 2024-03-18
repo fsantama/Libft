d0f03a9920f3d7086dcfe7b1195565e5f0e81889
fsantama <fransantamaria95@gmail.com>
2024-03-18 17:01:52 +0100

    libft.h actualizado

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fsantama <fsantama@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 11:07:31 by fsantama          #+#    #+#             */
-/*   Updated: 2023/06/13 14:46:34 by fsantama         ###   ########.fr       */
+/*   Updated: 2024/03/18 17:01:36 by fsantama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, int n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
